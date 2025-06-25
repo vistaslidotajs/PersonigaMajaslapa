4ea1850368a00857f1bb5e5d9ceee8a2c3979ae1
Filips <132749697+vistaslidotajs@users.noreply.github.com>
2025-06-25 04:15:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-const gads = new Date().getFullYear();
-document.getElementById("copyright").innerHTML = `&copy; ${gads} Filips Bībers`;
+window.onload = function () {
+  const gads = new Date().getFullYear();
+  document.getElementById("copyright").innerHTML = `© ${gads} Filips Bībers`;
+
+  const toggleButton = document.getElementById("toggle-dark-mode");
+
+  toggleButton.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+  });
+};
